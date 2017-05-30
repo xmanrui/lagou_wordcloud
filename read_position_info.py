3e9b95f0c0ee877f88940d046cc4a2915da3266f
xiemanrui@foxmail.com <xiemanrui@foxmail.com>
2017-05-31 01:36:19 +0800

    增加一个从excel表获取工资的函数

--- read_position_info.py
+++ read_position_info.py
@@ -1,18 +1,26 @@
 # -*- coding-8 -*-
 from openpyxl import load_workbook
 import os
+from common import get_job_type_from_position_info_xlsx
 
 
 def get_company_ids(xlsx_file):
-    basename = os.path.basename(xlsx_file)
-    str_split = basename.split('_')
-    lang_name = str_split[0]
+    job_type = get_job_type_from_position_info_xlsx(xlsx_file)
     wb = load_workbook(xlsx_file)
-    ws = wb.get_sheet_by_name(lang_name)
+    ws = wb.get_sheet_by_name(job_type)
     rows = len(list(ws.rows))
     list_id = [ws['L%s' % i].value for i in range(1, rows+1)]
 
     return list_id
+
+
+def get_salary_list(xlsx_file):
+    job_type = get_job_type_from_position_info_xlsx(xlsx_file)
+    wb = load_workbook(xlsx_file)
+    ws = wb.get_sheet_by_name(job_type)
+    rows = len(list(ws.rows))
+    salary_list = [ws['E%s' % i].value for i in range(1, rows+1)]
+    return salary_list
 
 
 def test_get_company_id():
