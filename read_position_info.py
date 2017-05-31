49d4d958d6d42b2a829c17dd7cbd7eb6f826fdcc
xiemanrui@foxmail.com <xiemanrui@foxmail.com>
2017-05-31 09:38:34 +0800

    增加一个读取区域信息的函数

--- read_position_info.py
+++ read_position_info.py
@@ -4,23 +4,33 @@ import os
 from common import get_job_type_from_position_info_xlsx
 
 
-def get_company_ids(xlsx_file):
+def get_common(xlsx_file, row_name):
     job_type = get_job_type_from_position_info_xlsx(xlsx_file)
     wb = load_workbook(xlsx_file)
     ws = wb.get_sheet_by_name(job_type)
     rows = len(list(ws.rows))
-    list_id = [ws['L%s' % i].value for i in range(1, rows+1)]
+    row_format = row_name + '%s'
+    content = [ws[row_format % i].value for i in range(1, rows+1)]
 
-    return list_id
+    return content
+
+
+def get_company_ids(xlsx_file):
+    ids_list = get_common(xlsx_file, 'L')  # L列是companyID
+    ids_list = filter(lambda x: x, ids_list)
+    return list(ids_list)
 
 
 def get_salary_list(xlsx_file):
-    job_type = get_job_type_from_position_info_xlsx(xlsx_file)
-    wb = load_workbook(xlsx_file)
-    ws = wb.get_sheet_by_name(job_type)
-    rows = len(list(ws.rows))
-    salary_list = [ws['E%s' % i].value for i in range(1, rows+1)]
-    return salary_list
+    salary_list = get_common(xlsx_file, 'E')  # E列是salary
+    salary_list = filter(lambda x: x, salary_list)  # 过滤掉None
+    return list(salary_list)
+
+
+def get_district(xlsx_file):
+    district_list = get_common(xlsx_file, 'I')
+    district_list = filter(lambda x: x, district_list)  # 过滤掉None
+    return list(district_list)
 
 
 def test_get_company_id():
@@ -28,5 +38,12 @@ def test_get_company_id():
     a = get_company_ids(test_file)
     print(a)
 
+
+def test_get_district():
+    test_file = './xlsx_file/python_position_info.xlsx'
+    a = get_district(test_file)
+    print(a)
+
 if __name__ == '__main__':
-    test_get_company_id()
+    # test_get_company_id()
+    test_get_district()
