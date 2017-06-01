9ca9135d77309b484343959cdc695fff70339008
xiemanrui@foxmail.com <xiemanrui@foxmail.com>
2017-06-02 02:05:14 +0800

    实现显示工资上下限平均值柱状图

--- statistics.py
+++ statistics.py
@@ -1,11 +1,14 @@
 
 import re
-from openpyxl import load_workbook
-from common import get_job_type_from_position_info_xlsx
+from common import get_job_type_from_position_info_xlsx, set_show_Chinese
 from read_position_info import get_salary_list
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
+set_show_Chinese()
 
-def calulate_average_salary(xlsx_file):
+def calculate_average_salary(xlsx_file):
     salary_list = get_salary_list(xlsx_file)
     text = ' '.join(salary_list)
     lower_limit = r"(\d+)k-"
@@ -19,42 +22,78 @@ def calulate_average_salary(xlsx_file):
     average_upper_limit = sum(results_upper_limit) / len(results_upper_limit)
     job_type = get_job_type_from_position_info_xlsx(xlsx_file)
 
-    return (average_lower_limit, average_upper_limit)
+    return job_type, average_lower_limit, average_upper_limit
 
 
-def test_calulate_average_salary():
-    # job_type = 'java'
-    # xlsx = './xlsx_file/%s_position_info.xlsx' % job_type
-    # calulate_average_salary(xlsx)
-    #
-    # job_type = 'python'
-    # xlsx = './xlsx_file/%s_position_info.xlsx' % job_type
-    # calulate_average_salary(xlsx)
-    #
-    # job_type = 'c++'
-    # xlsx = './xlsx_file/%s_position_info.xlsx' % job_type
-    # calulate_average_salary(xlsx)
+def plot_average_salary_histogram(job_list):
+    job_list = [job.lower() for job in job_list]
+    xlsx_list = ['./xlsx_file/%s_position_info.xlsx' % job for job in job_list]
+    r = []
+    for xlsx in xlsx_list:
+        t = calculate_average_salary(xlsx)
+        r.append(t)
 
+    r.sort(key=lambda x: x[1])
+
+    jobs = []
+    lower_limits = []
+    upper_limits = []
+    offset_list = []
+    for item in r:
+        jobs.append(item[0])
+        lower_limits.append(item[1])
+        upper_limits.append(item[2])
+        offset_list.append(item[2] - item[1])
+
+    r_copy = r[:]
+    r_copy.sort(key=lambda x: x[2], reverse=True)
+    [print('%s: %0.2fk - %0.2fk' % (x[0], x[1], x[2])) for x in sorted(r[:], key=lambda y: y[2], reverse=True)]
+    y_max = r_copy[0][2] + 5
+    opacity = 0.8
+    bar_width = 0.7
+    index = np.arange(0, len(r)*2, 2)
+
+    ymajor_locator = MultipleLocator(2)  # 将y轴主刻度标签设置为2的倍数
+    # ymajor_formatter = FormatStrFormatter('%1.1f')  # 设置y轴标签文本的格式
+    yminor_locator = MultipleLocator(0.5)  # 将此y轴次刻度标签设置为0.5的倍数
+    ax = plt.gca()
+    ax.yaxis.set_major_locator(ymajor_locator)
+    ax.yaxis.set_minor_locator(yminor_locator)
+
+    # ax.yaxis.set_major_formatter(ymajor_formatter)
+    #plt.bar(index, lower_limits, bar_width, alpha=opacity, color='b', label='下限平均值')
+    #plt.bar(index + bar_width, upper_limits, bar_width, alpha=opacity, color='r', label='上限平均值')
+
+    plt.bar(index + bar_width/2, offset_list, bar_width, bottom=lower_limits, alpha=opacity, color='r')
+
+    for i, v in enumerate(index):
+        lower = '%0.1fk' % r[i][1]
+        upper = '%0.1fk' % r[i][2]
+        offset = bar_width/5 * 2
+        plt.text(v + offset, r[i][1] - 1, lower)
+        plt.text(v + offset, r[i][2] + 0.5, upper)
+
+    plt.xlabel('职位')
+    plt.ylabel('工资')
+    plt.title('工资上下限平均值')
+    plt.xticks(index + bar_width, jobs)
+    plt.ylim(0, y_max)
+    plt.grid(True, axis='y')
+    plt.legend()
+    plt.savefig('./img_wordcloud/salary_histogram.png')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def test_calculate_average_salary():
     job_list1 = ['java', 'python', 'c++', 'c#', 'cocos2d-x', 'unity3d', '安卓', '前端', 'ios', 'opengl']
-    job_list = ['爬虫', '测试', '嵌入式', 'JavaScript', '自然语言处理', '数据挖掘', '机器学习', '大数据', '产品', '需求', '运营']
-    job_list.extend(job_list1)
-    r = list()
-    for job in job_list:
-        xlsx = './xlsx_file/%s_position_info.xlsx' % job.lower()
-        job_type = get_job_type_from_position_info_xlsx(xlsx)
-        t = calulate_average_salary(xlsx)
-        a = [job_type]
-        a.extend(t)
-        r.append(a)
-
-    r.sort(key=lambda x: x[1], reverse=True)
-    print(r)
-    for i in r:
-        s = '%s: %0.2fk - %0.2fk' % (i[0], i[1], i[2])
-        print(s)
+    # job_list = ['爬虫', '测试', '嵌入式', 'JavaScript', '自然语言处理', '数据挖掘', '机器学习', '大数据', '产品', '需求', '运营']
+    # job_list.extend(job_list1)
+    plot_average_salary_histogram(job_list1)
 
 
 if __name__ == '__main__':
-    test_calulate_average_salary()
+    test_calculate_average_salary()
 
 
